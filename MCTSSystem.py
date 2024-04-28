9bd5aaf9b0ddb725082a172ae4e51249ae917fb1
bjorneme <bjorn.melaaen@gmail.com>
2024-04-28 15:11:05 +0200

    feat: Create a ReplayBuffer

--- MCTSSystem.py
+++ MCTSSystem.py
@@ -1,19 +1,21 @@
+import copy
 import numpy as np
 from MCTS import MCTS, MCTSNode
+from ReplayBuffer import ReplayBuffer
 from games.Hex import Hex
 from games.TicTacToe import TicTacToe
 
 
 class MCTSSystem:
     def __init__(self):
-        pass
+        self.replay_buffer = ReplayBuffer()
 
     def self_play(self):
         # For counting win/ draw/ loss
         win, draw, loss = 0, 0, 0
 
         # Iterate over all the episodes
-        for episode in range(10):
+        for episode in range(1):
 
             # Initialize the game
             state_manager = TicTacToe()
@@ -26,14 +28,20 @@ class MCTSSystem:
             if(episode % 2 == 0):
                 state_manager.current_player = -1
 
+            buffer = []
+
             # Play until game is over
             while not state_manager.is_game_over():
 
                 # Execute the MCTS search. Get probabilities for possible actions
-                action_probs = mcts.run_simulation(1000)
+                action_probs = mcts.run_simulation(10000)
+                board_state = copy.deepcopy(state_manager.board)
 
                 # Select the best action based on the action probabilities
                 best_action = np.argmax(action_probs)
+
+                # Save to a buffer, before loading into replay buffer
+                buffer.append((board_state, action_probs, state_manager.current_player))
 
                 # Execute the best action
                 state_manager.make_move(best_action)
@@ -51,16 +59,13 @@ class MCTSSystem:
                 loss += 1
             print(f"Episode {episode}")
 
+            # Append game to replay buffer
+            for board_state, action_probs, player in buffer:
+                self.replay_buffer.push_to_buffer(board_state, action_probs, result, player)
+
         # Print final results
         print("Final Results:")
         print(f"Wins: {win}, Draws: {draw}, Losses: {loss}")
-
-
-
-
-
-
-
 
 
     def run_system(self):
---
+++ ReplayBuffer.py
@@ -0,0 +1,20 @@
+
+
+# Replaybuffer: store samples from self play
+class ReplayBuffer:
+    def __init__(self, max_size = 10000):
+        self.max_size = max_size # Max size replay buffer
+        self.buffer = [] # The replay buffer
+        self.position = 0 # Position in the buffer
+
+    def push_to_buffer(self, board_state, action_probs, value, player):
+        if len(self.buffer) < self.max_size:
+            self.buffer.append(None)
+
+        # Save to the buffer
+        self.buffer[self.position] = (board_state, action_probs, value, player)
+        self.position = (self.position + 1) % self.max_size
+
+    def get_sample(self, batch_size):
+        # Retrieve a batch of elements randomly
+        pass
